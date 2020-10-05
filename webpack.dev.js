0386334e6310f59595a13a54b1e3ff0e4743f8ad
Jerry Yip <jerry.yip@gmail.com>
2020-10-05 14:02:02 -0700

    exercise 1

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,11 +6,15 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     devtool: 'source-map',
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
